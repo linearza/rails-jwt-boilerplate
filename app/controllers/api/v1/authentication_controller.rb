f28bf1f7498350ca5e490bb4666209f90e4f5f9e
Michiel <linearza@users.noreply.github.com>
2019-09-26 21:38:24 +0200

    Adds additional auth check to make sure account is confirmed

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -3,15 +3,20 @@ module Api
     class AuthenticationController < ApplicationController
      skip_before_action :authenticate_request
 
-     def authenticate
-       command = AuthenticateUser.call(params[:email], params[:password])
+      def authenticate
+        command = AuthenticateUser.call(params[:email], params[:password])
 
-       if command.success?
-         render json: { auth_token: command.result }
-       else
-         render json: { error: command.errors }, status: :unauthorized
-       end
-     end
+        if command.success?
+          if User.where(email: params[:email]).first.confirmed
+            render json: { auth_token: command.result }
+          else
+            render json: { error: 'Account not yet confirmed.'}, status: :unauthorized
+          end
+
+        else
+           render json: { error: command.errors }, status: :unauthorized
+        end
+      end
     end
   end
 end
